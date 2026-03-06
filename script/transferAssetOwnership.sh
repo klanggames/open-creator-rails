bc982ecdc31b0736e73bfd0e484a1a0dc7c82fc3
rob1997 <robelgetnetrg@gmail.com>
2026-03-06 19:46:35 +0300

    transferAssetOwnership.sh

--- script/transferAssetOwnership.sh
+++ script/transferAssetOwnership.sh
@@ -8,14 +8,14 @@ source ./script/utils.sh
 
 registry_index=$1
 asset_id=$(cast keccak $2)
-new_subscription_price=$3
-asset_owner_private_key=$4
+asset_owner_private_key=$3
+new_owner=$4
 
 deployments_file=$(get_deployments_file)
 
 asset_address=$(jq -r ".[$registry_index].assets[] | select(.assetIdHash == \"$asset_id\") | .address" "$deployments_file")
 
-result=$(cast send $asset_address "setSubscriptionPrice(uint256)" $new_subscription_price --rpc-url $RPC_URL --private-key $asset_owner_private_key --json)
+result=$(cast send $asset_address "transferOwnership(address)" $new_owner --rpc-url $RPC_URL --private-key $asset_owner_private_key --json)
 EXIT_CODE=$?
 
 if [ $EXIT_CODE -ne 0 ]; then
@@ -26,11 +26,11 @@ transaction_hash=$(echo $result | jq -r '.transactionHash')
 
 jq --argjson registryIndex "$registry_index" \
    --arg assetIdHash "$asset_id" \
-   --argjson newSubscriptionPrice "$new_subscription_price" \
-   '.[$registryIndex].assets |= map(if .assetIdHash == $assetIdHash then .subscriptionPrice = $newSubscriptionPrice else . end)' \
+   --arg newOwner "$new_owner" \
+   '.[$registryIndex].assets |= map(if .assetIdHash == $assetIdHash then .owner = $newOwner else . end)' \
    "$deployments_file" > tmp.json && mv tmp.json "$deployments_file"
 
 echo "Asset ID: $2
 Details:
-  Subscription Price: $new_subscription_price
+  Owner: $new_owner
   Transaction Hash: $transaction_hash"
